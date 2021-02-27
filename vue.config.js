be9f7f1684349d0f972e5a5292ff82d2d219f6da
chriscauley <chris@lablackey.com>
2021-02-27 13:13:57 +0000

    npm package files

---
+++ vue.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  configureWebpack: {
+    externals: {
+      '@popperjs/core': {
+        root: 'Popper',
+        commonjs: '@popperjs/core',
+        commonjs2: '@popperjs/core',
+      },
+    },
+  },
+};
+
