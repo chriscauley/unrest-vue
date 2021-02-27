ba06390c973bd95edce4e73324d165585eaede6c
chriscauley <chris@lablackey.com>
2021-02-27 13:28:47 +0000

    FocusMixin

---
+++ src/FocusMixin.js
@@ -0,0 +1,20 @@
+export default {
+  data: () => ({ focused: false }),
+  methods: {
+    onFocus() {},
+    onBlur() {},
+    toggleFocus(e) {
+      if (e.__dropdown_touched !== this) {
+        e.__dropdown_touched = e.__dropdown_touched || this
+        if (this.focused) {
+          document.removeEventListener('click', this.toggleFocus)
+          this.onFocus()
+        } else if (!e.shiftKey && !e.ctrlKey) { // ctrl+shift+click locks dropdown open (for debugging)
+          document.addEventListener('click', this.toggleFocus)
+          this.onBlur()
+        }
+        this.focused = !this.focused
+      }
+    },
+  },
+}
